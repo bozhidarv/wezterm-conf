60b3617ce8b1210b3dffedf72052b5ca0f8d93b3
Bozhidar Videnov <bozh.bv03@gmail.com>
2025-01-29 09:34:14 +0200

    refactor(sessionizer): refactor sessionizer to make it better crossplatform

--- sessionizer.lua
+++ sessionizer.lua
@@ -8,27 +8,26 @@ local M = {}
 M.toggle = function(window, pane)
   local projects = {}
 
-  local success, stdout, stderr = wezterm.run_child_process {
-    'fd',
-    '--min-depth',
-    '1',
-    '--max-depth',
-    '3',
-    '--type',
-    'd',
-    '.',
-    'D:\\',
-    'C:\\Users\\BozhidarVidenov\\AppData',
-    'C:\\Users\\BozhidarVidenov\\.config',
-    '/home/bozzkoo/.config/',
-    '/home/bozzkoo/dotfiles/.config/',
-    '/home/bozzkoo/Documents/personal/',
-    '/home/bozzkoo/Documents/work/',
-    '/home/bozzkoo/Documents/uni/',
+  --- D:\\',
+  -- 'C:\\Users\\BozhidarVidenov\\AppData',
+  -- 'C:\\Users\\BozhidarVidenov\\.config',
+
+  ---@type string[]
+  local program = {
+    'sh',
+    '-c',
+    wezterm.config_dir .. '/scripts/sessionizer.sh',
   }
+
+  if wezterm.target_triple == 'x86_64-pc-windows-msvc' or wezterm.target_triple == 'x86_64-pc-windows-gnu' then
+    program = {}
+  end
+
+  local success, stdout, stderr = wezterm.run_child_process(program)
 
   if not success then
     wezterm.log_error('Failed to run fd: ' .. stderr)
+    wezterm.log_error(wezterm.config_dir)
     return
   end
 
@@ -36,13 +35,19 @@ M.toggle = function(window, pane)
     local project = line:gsub('/.git.*$', '')
     local label = project:sub(1, -2)
     wezterm.log_info(label)
-    local id = label:gsub('.*\\', '')
-    wezterm.log_info(id)
+
+    local id = ''
+    if wezterm.target_triple == 'x86_64-pc-windows-msvc' or wezterm.target_triple == 'x86_64-pc-windows-gnu' then
+      id = label:gsub('.*\\', '')
+    else
+      id = label:gsub('.*/', '')
+    end
     table.insert(projects, { label = tostring(label), id = tostring(id) })
   end
 
   window:perform_action(
     act.InputSelector {
+      ---@diagnostic disable-next-line: param-type-mismatch, missing-parameter
       action = wezterm.action_callback(function(win, _, id, label)
         if not id and not label then
           wezterm.log_info 'Cancelled'
